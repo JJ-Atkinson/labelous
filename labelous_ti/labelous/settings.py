dcf99f4934118bcf9b40f5eb4427fa1fc1fe91b5
jarrett <jj_atkinson@att.net>
2020-02-26 00:37:36 -0600

    Try to add templates to the code. Don't complain much if some config is wrong -- running blind right now. I couldn't config the server to launch on my computer yet.

--- labelous_ti/labelous/settings.py
+++ labelous_ti/labelous/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'labelous.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["./../label_app/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
